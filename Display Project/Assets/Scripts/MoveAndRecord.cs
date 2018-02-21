8458f9fb52b0126cb63331ac0798024bec3dc6a7
Igor Couto <igor.fcouto@gmail.com>
2018-02-20 22:34:09 -0300

    Initial Commit

---
+++ Display Project/Assets/Scripts/MoveAndRecord.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class MoveAndRecord : MonoBehaviour {
+
+	public float speed;
+
+	Vector3 startRotation;
+	Record record;
+
+	void Awake(){
+		// Lock at 30 fps
+		QualitySettings.vSyncCount = 0;
+		Application.targetFrameRate = 30;
+		// Get component that will record for us
+		record = Camera.main.gameObject.GetComponent<Record> ();
+		// Store start rotation
+		startRotation = transform.eulerAngles;
+	}
+
+	void FixedUpdate () {
+		// Rotate
+		transform.Rotate (0f, speed,0f);
+		// Check if has completed a 360 rotation
+		if( transform.eulerAngles.y == 0f || Vector3.Distance(transform.eulerAngles, startRotation) < 0.1f){
+			// Make sure to stop at right position
+			transform.rotation = Quaternion.Euler (0f,0f,0f);
+			// Stop recording
+			record.StopRecording ();
+			// Stop this script
+			enabled = false;
+		}
+	}
+}
